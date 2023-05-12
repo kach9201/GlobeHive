03d607054a476b6b673d92797890b242110a695a
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-05-11 20:24:56 -0400

    adding some stuff

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import CommonSection from '../shared/commonSection';
+import TourCard from '../shared/tourCard';
+import SearchBar from '../shared/searchBar';
+import Newsletter from '../shared/newsletter';
+import '../styles/tour.css';
+import tourData from '../assets/data/tours';
+import { Container } from 'reactstrap';
  
 const Tours = () => {
     return (
-        <div>Tours</div>
+     <>
+     <CommonSection title={"All Tours"} />
+     <section >
+         <Container>
+
+           <Row>
+
+            <SearchBar/>
+
+        </Row>
+      </Container>
+     </section>
+
+     <section>
+        
+         <Container>
+            <Row>
+
+         </Row>
+         </Container>
+         </section>
+     </>   
     )
 }
 
